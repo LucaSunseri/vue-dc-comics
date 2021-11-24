1ef93cf1bf06edba47349386a6b2f82fb734c6dd
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2021-11-24 16:10:10 +0100

    end header

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <template>
   <div>
-
+    <Header />
+    <Main />
+    <Footer />
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    Header,
+    Main,
+    Footer  
   }
 }
 </script>
 
 <style lang="scss">
-
+  @import "./assets/style/vars.scss";
+  @import "./assets/style/general.scss";
 </style>
