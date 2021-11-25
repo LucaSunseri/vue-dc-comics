db75c645a00bbc95ad267e5cb174a72f1a13b423
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2021-11-25 12:53:13 +0100

    Added new component Card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,15 +1,47 @@
 <template>
-  <div>
-
+  <div class="card">
+    <div class="card__img">
+      <img :src="contentCard.thumb" :alt="contentCard.series">
+    </div>
+    <div class="card__text">
+      <p>{{contentCard.series}}</p>
+    </div>
   </div> 
 </template>
 
 <script>
 export default {
-
+  name: 'Card',
+  props: {
+    contentCard: Object,
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+
+.card {
+  width: 150px;
+  display: flex;
+  flex-direction: column;
+}
+
+.card__img {
+  width: 100%;
+  height: 150px;
+  overflow: hidden;
+  img {
+    width: 100%;
+  }
+}
+
+.card__text {
+  margin-top: 10px;
+  p {
+    text-transform: uppercase;
+    color: #fff;
+    font-size: 12px;
+  }
+}
 
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,21 +1,74 @@
 <template>
   <main>
-    <h2>...Content goes here...</h2>
+    <div class="container">
+
+      <div class="title">
+        <h2>CURRENT SERIES</h2>
+      </div>
+
+      <div class="series">
+        <div v-for="(card, index) in contentCards" :key="index" class="series__box">
+          <Card :contentCard="card" />
+        </div>
+      </div>
+
+      <div class="button">
+        <span>LOAD MORE</span>
+      </div>
+
+    </div>
   </main> 
 </template>
 
 <script>
+import Card from './Card.vue';
+import contentCards from '../assets/data/ArrayCardMain.js'
+
 export default {
-  name: 'Main'
+  name: 'Main',
+  components: {
+    Card 
+  },
+  data() {
+    return {
+      contentCards
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+@import '../assets/style/vars.scss';
 
 main {
-  padding: 50px;
-  background-color: black;
-  color: #fff;
+  background-color: rgba(0, 0, 0, 0.836);
+  .title {
+    position: relative;
+    z-index: 100;
+    margin-top: -25px;
+    h2 {
+      display: inline-block;
+      background-color: $primary-color;
+      font-weight: 900;
+      padding: 10px 20px;
+      color: #fff;
+    }
+  }
+
+  .series {
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 30px;
+    padding: 50px 10px;
+  }
+
+  .series__box {
+    display: flex;
+    justify-content: center;
+    flex-basis: calc(100% / 6);
+    padding: 10px 0;
+  }
+
 }
 
 </style>
