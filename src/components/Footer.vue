6a9b6e397dd9776ef5aad63b18dab2f639b7c6e9
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2021-11-25 11:42:06 +0100

    section jumbotron

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -94,7 +94,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import "../assets/style/vars.scss";
 
 .footer__top {
---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <div>
+    <img src="../assets/img/jumbotron.jpg" alt="Cartoon">
+  </div>
+  
+</template>
+
+<script>
+export default {
+
+  name: 'Jumbotron'
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+div {
+  width: 100%;
+  max-height: 350px;
+  overflow: hidden;
+  img {
+    width: 100%;
+  }
+}
+
+</style>
